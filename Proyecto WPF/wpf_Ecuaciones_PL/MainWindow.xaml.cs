42ce344f94abc0d70d393b9cf1cf1468b62010b1
Cristopher Robles <crisrc012@hotmail.com>
2018-07-11 02:22:59 -0600

    Cambio de nombres de controles y eventos

--- Proyecto WPF/wpf_Ecuaciones_PL/MainWindow.xaml.cs
+++ Proyecto WPF/wpf_Ecuaciones_PL/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using cls_BLL.Controles;
 using cls_DAL.Controles;
 
@@ -31,43 +20,46 @@ namespace wpf_Ecuaciones_PL
 
         private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            this.DragMove();
+            DragMove();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void txt_NumA_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            txt_NumA.Text = string.Empty;
-            txt_NumB.Text = string.Empty;
-            txt_NumC.Text = string.Empty;
-            txt_Resultado.Text = string.Empty;
-            txt_Resultado1.Text = string.Empty;
-            txt_Resultado2.Text = string.Empty;
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            if (txt_NumA.Text == "" || txt_NumB.Text == "" || txt_NumC.Text == "")
+            CultureInfo cultura = CultureInfo.CurrentCulture;
+            var FormatoNumero = cultura.NumberFormat;
+            string sSepDecimal = FormatoNumero.NumberDecimalSeparator.ToString().Trim();
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9 || 
+                e.Key.Equals(Key.Subtract) || e.Key.Equals(sSepDecimal))
             {
-                System.Windows.Forms.MessageBox.Show("Es necesario indicar todos los valores","Datos faltantes", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                
+                e.Handled = false;
             }
             else
             {
+                e.Handled = true;
+            }
+        }
 
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_NumA.Text == "" || txt_NumB.Text == "" || txt_NumC.Text == "")
+            {
+                System.Windows.Forms.MessageBox.Show("Es necesario indicar todos los valores",
+                    "Datos faltantes", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
                 cls_Ecuaciones_DAL ObjEcuDal = new cls_Ecuaciones_DAL();
                 cls_Ecuaciones_BLL ObjEcuBLL = new cls_Ecuaciones_BLL();
-
                 ObjEcuDal.fNumA = Convert.ToSingle(txt_NumA.Text);
                 ObjEcuDal.fNumB = Convert.ToSingle(txt_NumB.Text);
                 ObjEcuDal.fNumC = Convert.ToSingle(txt_NumC.Text);
                 ObjEcuBLL.Discriminante(ref ObjEcuDal);
                 txt_Resultado.Text = Convert.ToString(Math.Round((ObjEcuDal.fDiscriminante), 2));
-
                 if (ObjEcuDal.fDiscriminante < 0)
                 {
                     System.Windows.Forms.MessageBox.Show("La ecuación no tiene solución");
@@ -91,24 +83,14 @@ namespace wpf_Ecuaciones_PL
             }
         }
 
-        private void Button_Click_3(object sender, RoutedEventArgs e)
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
-        }
-
-        private void txt_NumA_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            CultureInfo cultura = CultureInfo.CurrentCulture;
-            var FormatoNumero = cultura.NumberFormat;
-            string sSepDecimal = FormatoNumero.NumberDecimalSeparator.ToString().Trim();
-            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9 || e.Key.Equals(Key.Subtract) || e.Key.Equals(sSepDecimal))
-            {
-                e.Handled = false;
-            }
-            else
-            {
-                e.Handled = true;
-            }
+            txt_NumA.Text = string.Empty;
+            txt_NumB.Text = string.Empty;
+            txt_NumC.Text = string.Empty;
+            txt_Resultado.Text = string.Empty;
+            txt_Resultado1.Text = string.Empty;
+            txt_Resultado2.Text = string.Empty;
         }
     }
 }
